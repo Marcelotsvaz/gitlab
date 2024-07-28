99fe2f87f253f0226482942127e55ce04cdb4d64
Marcelo Tellier Sartori Vaz <marcelotsvaz@gmail.com>
2024-07-28 19:05:18 -0300

    Delete runner cache archive after 30 days

--- cicd_infrastructure/cache.tf
+++ cicd_infrastructure/cache.tf
@@ -8,11 +8,25 @@ resource aws_s3_bucket runner_cache {
 }
 
 
-resource aws_s3_bucket_public_access_block main {
+resource aws_s3_bucket_public_access_block runner_cache {
 	bucket = aws_s3_bucket.runner_cache.id
 	
 	block_public_acls = true
 	ignore_public_acls = true
 	block_public_policy = true
 	restrict_public_buckets = true
+}
+
+
+resource aws_s3_bucket_lifecycle_configuration runner_cache {
+	bucket = aws_s3_bucket.runner_cache.id
+	
+	rule {
+		id = "Delete Old Archives"
+		status = "Enabled"
+		
+		expiration {
+			days = 30
+		}
+	}
 }
